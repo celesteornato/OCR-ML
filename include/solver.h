c5c221b0dad5f9b779324194ac38e340a490fffa
Céleste Ornato <celeste@ornato.com>
2025-10-14 23:11:52 +0200

    solver{.c,.h}: refactoring and fixed const-correctness

--- include/solver.h
+++ include/solver.h
@@ -1,44 +1,31 @@
-#ifndef SOLVER_H
-#define SOLVER_H
-
-#include <stdio.h>
-#include <string.h>
+#ifndef SOLVER_H_
+#define SOLVER_H_
 
 #define MAX_SIZE 100
 
-
-
-struct Coordinates
-{
+/* Coordinates of the word searched :
+1st : column of the first char
+2nd : row of the first char
+3rd : column of last char
+4th : row of last char */
+struct coordinates {
     int start_x;
     int start_y;
     int end_x;
     int end_y;
 };
-/* 
-Coordinates of the word searched :
-1st : column of the first char
-2nd : row of the first char
-3rd : column of last char
-4th : row of last char
-*/
-extern struct Coordinates Word_Coordinates;
-//Our word's coordinates
 
-void go_directions(char grid[MAX_SIZE][MAX_SIZE], int index, char *word, int rows, int cols, int x, int y, int i, int j);
-/*
-Functiun called by search_the_word wich test in a given directions (described
+extern struct coordinates word_coordinates;
+
+/* Function called by search_the_word wich test in a given directions (described
 by the int i and j). If the word is found then we change the coordinates of
-the last char coordinates's. Nothing,otherwise.
-*/
-void search_the_word(char grid[MAX_SIZE][MAX_SIZE], int rows, int cols, char *word);
-/*
-Functiun that finds the word, if so it updates the coordinates of the first
-char of our word to the good one (The last coordinates are handleded by
-go_directions). Nothing, otherwise
-*/
+the last char coordinates's. Nothing,otherwise. */
+void go_directions(const char grid[MAX_SIZE][MAX_SIZE], int index, const char *word, int rows,
+                   int cols, int x, int y, int i, int j);
 
-#endif
+/* Function that finds the word, if so it updates the coordinates of the first
+char of our word to the good one (The last coordinates are handleded by go_directions). Otherwise,
+nothing */
+void search_the_word(const char grid[MAX_SIZE][MAX_SIZE], int rows, int cols, const char *word);
 
-
-
+#endif // SOLVER_H_
--- src/solver.c
+++ src/solver.c
@@ -1,4 +1,6 @@
-#include "solver.h"
+#include <solver.h>
+#include <stdio.h>
+#include <string.h>
 
 /*
 Rewind for Virgile (sacre Virgile !):
@@ -14,91 +16,88 @@ Rewind for Virgile (sacre Virgile !):
 {-1,-1} up-left
 */
 
-struct Coordinates Word_Coordinates = {-1, -1, -1, -1,};
+struct coordinates word_coordinates = {
+    -1,
+    -1,
+    -1,
+    -1,
+};
 
-void go_directions(char grid[MAX_SIZE][MAX_SIZE], int index, const char *word, int rows, int cols, int x, int y, int i, int j)
+void go_directions(const char grid[MAX_SIZE][MAX_SIZE], int index, const char *word, int rows,
+                   int cols, int x, int y, int i, int j)
 {
     int len = (int)strlen(word);
-    while(x >= 0 && y >= 0 && x<cols && y<rows && index< len && grid[y][x] == word[index])
+    while (x >= 0 && y >= 0 && x < cols && y < rows && index < len && grid[y][x] == word[index])
     {
-	    x += i;
-	    y += j;
-	    index++;
+        x += i;
+        y += j;
+        index++;
     }
-    if(index == len)
+    if (index == len)
     {
-	    Word_Coordinates.end_x = x-i;
-	    Word_Coordinates.end_y = y-j;
-	    return;
+        word_coordinates.end_x = x - i;
+        word_coordinates.end_y = y - j;
     }
-    return;
-
 }
 
-void search_the_word(char grid[MAX_SIZE][MAX_SIZE], int rows, int cols, const char *word)
+static void search_the_word(const char grid[MAX_SIZE][MAX_SIZE], int rows, int cols, const char *word)
 {
-    for(int y = 0; y< rows; y++)
+    for (int y = 0; y < rows; y++)
     {
-        for(int x = 0; x < cols; x++)
-	{
-	    if(grid[y][x] != word[0])
-	    {
-		    continue; //Not the 1st char like wtf why should I care
-	    }
-	    for(int i = -1; i<= 1; i++) //going up/nothing/down
-	    {
-		    for(int j = 0; j<= 1; j++) //going left/nothing/right
-		    {
-			if(i==0 && j==0) //if there's no move quoi like wtf I am not paid to work for nothing
-			{ continue; }
-			Word_Coordinates.end_x = -1;
-			go_directions(grid, 1, word, rows, cols, x+i, y+j, i, j);
-			if(Word_Coordinates.end_x != -1) //if it has been find
-			{
-				Word_Coordinates.start_x = x;
-				Word_Coordinates.start_y = y;
-				printf("(%d,%d),(%d,%d)", Word_Coordinates.start_x, Word_Coordinates.start_y, Word_Coordinates.end_x, Word_Coordinates.end_y);
-				return;
-			}
-		    }
-	    }
-	}
+        for (int x = 0; x < cols; x++)
+        {
+            if (grid[y][x] != word[0])
+            {
+                continue; // Not the 1st char like wtf why should I care
+            }
+            for (int i = -1; i <= 1; i++) // going up/nothing/down
+            {
+                for (int j = 0; j <= 1; j++) // going left/nothing/right
+                {
+                    // if there's no move quoi like wtf I am not paid to work for nothing
+                    if (i == 0 && j == 0)
+                    {
+                        continue;
+                    }
+                    word_coordinates.end_x = -1;
+                    go_directions(grid, 1, word, rows, cols, x + i, y + j, i, j);
+                    if (word_coordinates.end_x != -1) // if it has been find
+                    {
+                        word_coordinates.start_x = x;
+                        word_coordinates.start_y = y;
+                        printf("(%d,%d),(%d,%d)\n", word_coordinates.start_x,
+                               word_coordinates.start_y, word_coordinates.end_x,
+                               word_coordinates.end_y);
+                        return;
+                    }
+                }
+            }
+        }
     }
     printf("Not found\n");
-    return; //no change (sad, we did not find our word)
 }
 
-void resolve(const char * list[], char grid[][MAX_SIZE], size_t length, int rows, int cols)
+void resolve(const char *list[], char grid[MAX_SIZE][MAX_SIZE], size_t length, int rows, int cols)
 {
-    for(size_t i = 0; i < length; i++)
+    for (size_t i = 0; i < length; i++)
     {
-	printf("Searching for %s\n", list[i]);
-	search_the_word( grid, rows, cols, list[i]);
+        printf("Searching for %s\n", list[i]);
+        search_the_word(grid, rows, cols, list[i]);
     }
 }
-
-
-
 
 int main(void)
 {
-    char grid[MAX_SIZE][MAX_SIZE]=
-    {
-	"DGHEYEUJKQIDIDIDIDID",
-	"FSING1QJJDAAAAAAAAAA",
-	"EYUTTFSODIAAAAHAAAAA",
-	"KIORKFLKLAAAAEAAAAAA",
-	"TEFOTOFRTLAALAAAAAAA",
-	"ASZPDPHKLSOLAAAAAAAA",
-	"ETYIGIKNXKOAAAAAAAAA",
-	"UKGCFCEDEDAAAAAAAAAA"
-    };
+    char grid[MAX_SIZE][MAX_SIZE] = {"DGHEYEUJKQIDIDIDIDID", "FSING1QJJDAAAAAAAAAA",
+                                     "EYUTTFSODIAAAAHAAAAA", "KIORKFLKLAAAAEAAAAAA",
+                                     "TEFOTOFRTLAALAAAAAAA", "ASZPDPHKLSOLAAAAAAAA",
+                                     "ETYIGIKNXKOAAAAAAAAA", "UKGCFCEDEDAAAAAAAAAA"};
 
     int rows = 8;
     int cols = 20;
-    char word[] = "HELLO";  //should give (2,13),(6,9)
-    char word2[] = "ENTROPIC"; //should give (0,3)(7,3)
-    char word3[] = "ING1"; //should give (1,2)(1,5)
+    char word[] = "HELLO";     // should give (2,13),(6,9)
+    char word2[] = "ENTROPIC"; // should give (0,3)(7,3)
+    char word3[] = "ING1";     // should give (1,2)(1,5)
 
     printf("\n");
     return 0;
