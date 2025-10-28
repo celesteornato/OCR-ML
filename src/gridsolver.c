bb6668ebc7103f5ad6e0101909e4c37581696d18
Céleste Ornato <celeste@ornato.com>
2025-10-28 18:38:10 +0100

    gridsolver: added tests

--- src/gridsolver.c
+++ src/gridsolver.c
@@ -1,7 +1,11 @@
+#include "neural.h"
 #include <SDL2/SDL.h>
 #include <assert.h>
 #include <grayscale.h>
 #include <solver.h>
+#include <stdint.h>
+#include <time.h>
+
 
 static int test_solver(void) {
   const char grid[][MAX_SIZE] = {
@@ -49,6 +53,44 @@ static int test_thresholding(char path[static 1]) {
   return 1;
 }
 
+static int test_p2bmap(const char path[static 1]) {
+  int dim = 32 / 8;
+  uint8_t a[(32 * 32) / 8];
+  if (!path_to_bitmap(path, a, 32, dim)) {
+    return 0;
+  }
+  for (int i = 0; i < 32; ++i) {
+    for (int j = 0; j < dim; ++j) {
+      for (int n = 7; n >= 0; --n) {
+        printf("%c", (((a[j + (i * dim)]) >> n) & 1) == 0 ? ' ' : '@');
+      }
+    }
+    printf("\n");
+  }
+  return 1;
+}
+
+static int test_neural(void) {
+  struct neural_network n = {1};
+  srand(time(NULL)); //NOLINT
+
+  train(&n);
+
+
+  for (size_t i = 0; i < INPUT_SIZE; ++i) {
+    printf("Input Node %zu : value of %f\n", i, n.input[i]);
+    for (size_t j = 0; j < LAYER1_SIZE; ++j) {
+      printf("\tWeight with node %zu : %f\n",j,n.layer1_weights[j][i]);
+    }
+  }
+
+  printf("%d\n", neural_find_logic(&n, true, false));
+  printf("%d\n", neural_find_logic(&n, false, true));
+  printf("%d\n", neural_find_logic(&n, true, true));
+  printf("%d\n", neural_find_logic(&n, false, false));
+  return 1;
+}
+
 int main(int argc, char *argv[]) {
   if (argc != 2) // error
   {
@@ -59,6 +101,8 @@ int main(int argc, char *argv[]) {
 
   assert(test_solver());
   assert(test_thresholding(argv[1]));
+  assert(test_p2bmap(argv[1]));
+  assert(test_neural());
 
   SDL_Quit();
 }
