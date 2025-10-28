75b4994bdf922abe715e6964b7140ffe9a75e6bb
Céleste Ornato <celeste@ornato.com>
2025-10-28 11:48:21 +0100

    solver.h added a few enums and fixed comments

--- include/solver.h
+++ include/solver.h
@@ -1,7 +1,11 @@
 #ifndef SOLVER_H_
 #define SOLVER_H_
 
-#define MAX_SIZE 100
+#include <stddef.h>
+enum { MAX_SIZE = 100 };
+
+enum direction_y { DIREC_UP = -1, DIREC_DOWN = 1 };
+enum direction_x { DIREC_LEFT = -1, DIREC_RIGHT = 1 };
 
 /* Coordinates of the word searched :
 1st : column of the first char
@@ -9,27 +13,31 @@
 3rd : column of last char
 4th : row of last char */
 struct coordinates {
-    int start_x;
-    int start_y;
-    int end_x;
-    int end_y;
+  int start_x;
+  int start_y;
+  int end_x;
+  int end_y;
 };
 
 extern struct coordinates word_coordinates;
 
 /* Function called by search_the_word wich test in a given directions (described
-by the int i and j). If the word is found then we change the coordinates of
+by i and j). If the word is found then we change the coordinates of
 the last char coordinates's. Nothing,otherwise. */
-void go_directions(const char grid[MAX_SIZE][MAX_SIZE], int index, const char *word, int rows,
-                   int cols, int x, int y, int i, int j);
+void go_directions(const char grid[MAX_SIZE][MAX_SIZE], size_t index,
+                   const char word[static 1], int rows, int cols, int x, int y,
+                   enum direction_x i, enum direction_y j);
 
 /* Function that finds the word, if so it updates the coordinates of the first
-char of our word to the good one (The last coordinates are handleded by go_directions). Otherwise,
-nothing */
-void search_the_word(const char grid[MAX_SIZE][MAX_SIZE], int rows, int cols, const char *word);
+char of our word to the good one (The last coordinates are handleded by
+go_directions). Otherwise, nothing */
+void search_the_word(const char grid[MAX_SIZE][MAX_SIZE], int rows, int cols,
+                     const char *word);
 
 /*Call the search of words on each word of the list that we all have to resolve.
  * (En resume) resolve the whole "mots caches"
-*/
-void resolve(const char *list[], const char grid[MAX_SIZE][MAX_SIZE], size_t length, int rows, int cols);
+ */
+void resolve(const char *list[], const char grid[MAX_SIZE][MAX_SIZE],
+             size_t length, int rows, int cols);
+
 #endif // SOLVER_H_
