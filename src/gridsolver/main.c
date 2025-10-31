0bbd41c3aa3cc04183657dea25a61ee82fa200f9
Céleste Ornato <celeste@ornato.com>
2025-10-31 17:12:21 +0100

    grid solver: made it subject compliant

--- src/gridsolver/main.c
+++ src/gridsolver/main.c
@@ -1,32 +1,72 @@
-#include <SDL2/SDL.h>
 #include <assert.h>
+#include <err.h>
+#include <fcntl.h>
 #include <solver.h>
+#include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-static int test_solver(void) {
-  const char grid[][MAX_SIZE] = {
-      "DGHEYEUJKQIDIDIDIDID", "FSING1QJJDAAAAAAAAAA", "EYUTTFSODIAAAAHAAAAA",
-      "KIORKFLKLAAAAEAAAAAA", "TEFOTOFRTLAALAAAAAAA", "ASZPDPHKLSOLAAAAAAAA",
-      "ETYIGIKNXKOAAAAAAAAA", "UKGCFCEDEDAAAAAAAAAA"};
+static int test_solver(const char *grid[static 1], const char *word, int rows,
+                       int cols) {
+  const char *list[] = {word};
 
-  int rows = 8;
-  int cols = 20;
-  char word[] = "HELLO";
-  char word2[] = "ENTROPIC";
-  char word3[] = "ING1";
-  const char *list[] = {word, word2, word3};
-
-  // Expected results: {(14,2),(10,6)} , {(3,0)(3,7)},  {(2,1)(5,1)}
-  resolve(list, grid, 3, rows, cols);
+  resolve(list, grid, sizeof(list)/sizeof(*list), rows, cols);
 
   printf("\n");
   return 1;
 }
 
-int main(void) {
-  SDL_Init(SDL_INIT_VIDEO);
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    printf("%d\n", argc);
+    return 1;
+  }
 
-  assert(test_solver());
+  struct stat st;
 
-  SDL_Quit();
+  int fd = open(argv[1], O_RDONLY);
+  if (fd < 0) {
+    errx(EXIT_FAILURE, "Error opening file %s", argv[1]);
+  }
+  if (fstat(fd, &st) == -1) {
+    goto error;
+  }
+
+  char *data = mmap(NULL, (size_t)st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+  if (data == NULL) {
+    goto error2;
+  }
+
+  int cols = 0;
+  while (data[cols] != '\n') {
+    cols += 1;
+  }
+  int rows = (int)(st.st_size /
+                   (cols + 1)); // cols+1 because we include the newline too
+
+  const char **grid = (const char **)calloc((size_t)rows, sizeof(char *));
+
+  for (int i = 0; i < rows; ++i) {
+    grid[i] = &data[(ptrdiff_t)((cols + 1) * i)];
+  }
+
+  printf("R: %d, C: %d\n", rows, cols);
+  test_solver(grid, argv[2], rows, cols);
+
+  free((void*)grid);
+  munmap(data, (size_t)st.st_size);
+  close(fd);
+  return 0;
+
+error2:
+  if (data != NULL) {
+    munmap(data, (size_t)st.st_size);
+  }
+error:
+  close(fd);
+  return EXIT_FAILURE;
 }
